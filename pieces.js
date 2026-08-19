61e4e5b8e955b1582411bdefc8fb6ea47e924632
phZ22700 <126951688+phZ22700@users.noreply.github.com>
2026-08-19 20:22:28 +0200

    Requête GET les boutons vis

--- pieces.js
+++ pieces.js
@@ -1,3 +1,5 @@
+// après ajout des boutons Afficher les avis
+import { ajoutListenersAvis } from "./avis.js";
 // Récupération des pièces depuis le fichier JSON
 const reponse = await fetch('pieces-autos.json');
 const pieces = await reponse.json();
@@ -23,6 +25,11 @@ function genererPieces(pieces){
         descriptionElement.innerText = article.description ?? "Pas de description pour le moment.";
         const stockElement = document.createElement("p");
         stockElement.innerText = article.disponibilite ? "En stock" : "Rupture de stock";
+
+        //Code ajouté
+        const avisBouton = document.createElement("button");
+        avisBouton.dataset.id = article.id;
+        avisBouton.textContent = "Afficher les avis";
         
         // On rattache la balise article a la section Fiches
         sectionFiches.appendChild(pieceElement);
@@ -34,9 +41,13 @@ function genererPieces(pieces){
         //Ajout des éléments au DOM pour l'exercice
         pieceElement.appendChild(descriptionElement);
         pieceElement.appendChild(stockElement);
-    
-     }
-}
+           //Code aJouté
+        pieceElement.appendChild(avisBouton);
+
+        ajoutListenersAvis();
+        }
+    }
+
 
 genererPieces(pieces);
 
