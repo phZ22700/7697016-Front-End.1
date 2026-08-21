c9f9f65bb513b0ace0cb00942fdc49aff42c9c71
phZ22700 <126951688+phZ22700@users.noreply.github.com>
2026-08-21 18:00:58 +0200

    Envoyer les avis OK

--- pieces.js
+++ pieces.js
@@ -1,9 +1,11 @@
 
-import { ajoutListenersAvis } from "./avis.js";
+import { ajoutListenersAvis, ajoutListenerEnvoyerAvis } from "./avis.js";
 
 // Récupération des pièces depuis le fichier JSON
 const reponse = await fetch('http://localhost:8081/pieces');
 const pieces = await reponse.json();
+ajoutListenerEnvoyerAvis()
+
 
 function genererPieces(pieces){
     for (let i = 0; i < pieces.length; i++) {
