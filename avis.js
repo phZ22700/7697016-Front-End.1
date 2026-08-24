19477e421fca86147243851923f2239358f2e1b2
phZ22700 <126951688+phZ22700@users.noreply.github.com>
2026-08-24 16:38:20 +0200

    factorisation : fonction afficherAvis

--- avis.js
+++ avis.js
@@ -6,31 +6,33 @@ export function ajoutListenersAvis() {
  
     for (let i = 0; i < piecesElements.length; i++) {
  
-     piecesElements[i].addEventListener("click", async function (event) {
+        piecesElements[i].addEventListener("click", async function (event) {
  
-      //  const id = event.target.dataset.id;
-      //  fetch(`http://localhost:8081/pieces/${id}/avis`);
-      // ajout-remplacement P3C2
-      //const id = event.target.dataset.id;
-      //const reponse = await fetch("http://localhost:8081/pieces/" + id + "/avis");
-      const id = event.target.dataset.id;
-      const reponse = await fetch("http://localhost:8081/pieces/" + id + "/avis");
-      const avis = await reponse.json();
-
-
-      const pieceElement = event.target.parentElement;
-      const avisElement = document.createElement("p");
-      for (let i = 0; i < avis.length; i++) {
-         avisElement.innerHTML += `${avis[i].utilisateur}: ${avis[i].commentaire} <br>`;
-         }
-         pieceElement.appendChild(avisElement)
+        // const id = event.target.dataset.id;
+        // fetch(`http://localhost:8081/pieces/${id}/avis`);
+        // ajout-remplacement P3C2
+        // const id = event.target.dataset.id;
+        // const reponse = await fetch("http://localhost:8081/pieces/" + id + "/avis");
+        const id = event.target.dataset.id;
+        const reponse = await fetch("http://localhost:8081/pieces/" + id + "/avis");
+        const avis = await reponse.json();
+        window.localStorage.setItem(`avis-piece-${id}`, JSON.stringify(avis))
+        const pieceElement = event.target.parentElement;
+        afficherAvis(pieceElement, avis)
      });
- 
     }
  
  }
+export function afficherAvis(pieceElement, avis){
+    const avisElement = document.createElement("p");
+    for (let i = 0; i < avis.length; i++) {
+        avisElement.innerHTML += `${avis[i].utilisateur} : ${avis[i].commentaire} -- ${avis[i].nbEtoiles} * <br>`
+    }
+    pieceElement.appendChild(avisElement)
 
- export function ajoutListenerEnvoyerAvis() {
+    }
+    
+export function ajoutListenerEnvoyerAvis() {
     const formulaireAvis = document.querySelector(".formulaire-avis");
     formulaireAvis.addEventListener("submit", function (event) {
     event.preventDefault();
